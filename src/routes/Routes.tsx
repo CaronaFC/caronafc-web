30a82a74f68699df14cb77536acd74bafebad5a2
hfidelis <heitorc88@gmail.com>
2025-06-26 00:02:35 -0300

    feat(routes): protected routes

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,12 +1,35 @@
-import { Routes, Route } from "react-router-dom";
+import { useAuth } from "../services/auth";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 import HomePage from "../pages/HomePage";
+import LoginPage from "../pages/LoginPage";
 import NotFoundPage from "../pages/NotFoundPage";
+import DashboardPage from "../pages/DashboardPage";
 
 export default function AppRoutes() {
+  const { isAuthenticated } = useAuth();
+
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route
+        path="/login"
+        element={
+        !isAuthenticated
+          ? <LoginPage />
+          : <Navigate to="/dashboard" replace />
+        }
+      />
+      <Route
+        path="/dashboard"
+        element={
+          isAuthenticated ? (
+            <DashboardPage />
+          ) : (
+            <Navigate to="/login" replace />
+          )
+        }
+      />
       <Route path="*" element={<NotFoundPage />} />
     </Routes>
   );
